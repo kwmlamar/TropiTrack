3f19ccdb47f5d1da9f7bbd791606371205f4685b
Lamar Sineus <134800327+kwmlamar@users.noreply.github.com>
2025-05-10 10:13:13 -0400

    feat: fixed login functionality and added login and signup pages

---
+++ components/signup-form.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { signup } from '@/app/actions/auth' // adjust path if needed
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create an account</CardTitle>
+          <CardDescription>
+            Enter your email and password to sign up
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={signup}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="you@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Sign Up
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Sign Up with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{' '}
+              <a href="/login" className="underline underline-offset-4">
+                Log in
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
