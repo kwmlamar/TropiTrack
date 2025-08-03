fd0c128ec63288176806df24ddff94b757ae02b1
kwmlamar <classicalsineus@gmail.com>
2025-08-03 19:13:55 -0400

    chore: fixed build erros

--- app/test-workers-step/page.tsx
+++ app/test-workers-step/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { StandaloneWorkersStep } from '@/components/onboarding/standalone-workers-step';
+import { OnboardingProvider } from '@/context/onboarding-context';
 
 export default function TestWorkersStepPage() {
-  return <StandaloneWorkersStep />;
+  return (
+    <OnboardingProvider>
+      <StandaloneWorkersStep />
+    </OnboardingProvider>
+  );
 } 
